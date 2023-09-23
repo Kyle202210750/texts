6872812284b991fa474a5bf8ad1b773546f15618
Kyle202210750 <133838256+Kyle202210750@users.noreply.github.com>
2023-09-23 19:36:54 +0800

    Create MathRushedProblem3.java

---
+++ MathRushedProblem3.java
@@ -0,0 +1,12 @@
+public class MathRushed {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double num1 = Math.cbrt(30);
+        
+        System.out.println(num1);
+        // Output: 3.1072325059538586
+    }
+}
