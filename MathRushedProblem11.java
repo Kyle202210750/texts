e388afd59dd85b2d5dbf7132fcdf4afc953d171c
Kyle202210750 <133838256+Kyle202210750@users.noreply.github.com>
2023-09-23 20:52:47 +0800

    Create MathRushedProblem11.java

---
+++ MathRushedProblem11.java
@@ -0,0 +1,15 @@
+public class MathRushedProblem11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // get -3 ^ -2
+        double num1 = Math.pow(-3.0, -2.0);
+        // Calculate the num1 and raised to the power of 2
+        double result = Math.pow(num1, 2.0);
+        
+        System.out.println(result);
+        // Output: 0.012345679012345678
+    }
+}
