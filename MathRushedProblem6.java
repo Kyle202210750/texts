4cd314ced9ee5fce4031cc5007050fd118888395
Kyle202210750 <133838256+Kyle202210750@users.noreply.github.com>
2023-09-23 19:56:15 +0800

    Create MathRushedProblem6.java

---
+++ MathRushedProblem6.java
@@ -0,0 +1,13 @@
+public class MathRushed {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Raising 2 to the power of 3/4, initiate result
+        double num1 = Math.ceil(Math.pow(2, (3.0 / 4.0)));
+        
+        System.out.println(num1);
+        // Output: 2.0
+    }
+}
