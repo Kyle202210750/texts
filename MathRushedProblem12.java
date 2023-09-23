011dd71d5f322227b685346af4defc2d59d3cd4f
Kyle202210750 <133838256+Kyle202210750@users.noreply.github.com>
2023-09-23 21:09:24 +0800

    Create MathRushedProblem12.java

---
+++ MathRushedProblem12.java
@@ -0,0 +1,24 @@
+public class MathRushed {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double value = 1024.0;
+        double base = 2.0;
+
+        // Calculate the logarithm base 2 of 1024
+        double num1 = Math.log(value) / Math.log(base);
+        // Rewrite 1024 as 2^logResult using the power rule
+        double num2 = Math.pow(base, num1);
+        // Apply the identity rule: 2^logResult = 1024
+        
+        System.out.println("Logarithm : " + num1);
+        System.out.println("Expression : " + num2);
+        System.out.println("Identity : " + value);
+        // Output:
+        // Logarithm : 10.0
+        // Expression : 1024.0
+        // Identity : 1024.0
+    }
+}
