bfb9613b24a99c805bc58295741d399de450a718
Kyle202210750 <133838256+Kyle202210750@users.noreply.github.com>
2023-09-23 19:47:33 +0800

    Create MathRushedProblem5.java

---
+++ MathRushedProblem5.java
@@ -0,0 +1,12 @@
+public class MathRushed {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double num1 = Math.ceil(-99.99);
+        
+        System.out.println(num1);
+        // Output: -99.0
+    }
+}
