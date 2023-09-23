85df9b16035e4fff9e29119e42bab1f64fb0b19f
Kyle202210750 <133838256+Kyle202210750@users.noreply.github.com>
2023-09-23 20:30:18 +0800

    Create MathRushedProblem10.java

---
+++ MathRushedProblem10.java
@@ -0,0 +1,15 @@
+public class MathRushedProblem10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Get 2 squared
+        double num1 = Math.pow(2.0, 2.0);
+        // Raise num1 to the power of 3
+        double result = Math.pow(num1, 3.0);
+        
+        System.out.println(result);
+        // Output: 64.0
+    }
+}
