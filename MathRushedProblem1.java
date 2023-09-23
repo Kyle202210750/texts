26f176467dc38f4d0257f34eb37ce0322e13a37b
Kyle202210750 <133838256+Kyle202210750@users.noreply.github.com>
2023-09-23 19:29:07 +0800

    Create MathRushedProblem1.java

---
+++ MathRushedProblem1.java
@@ -0,0 +1,12 @@
+public class MathRushedProblem1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double num1 = Math.floor(3.99);
+        
+        System.out.println(num1);
+        // Output: 3.0
+    }
+}
